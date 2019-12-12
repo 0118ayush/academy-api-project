8ec495553bc42de268d89d0a5c685b31a615066d
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-12-12 15:44:36 +0000

    crud functionality complete

--- express-api/app.js
+++ express-api/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = 5000;
+import db from "./db/db";
 
 import {
   getAllCars,
@@ -14,22 +15,35 @@ app.use(bodyParser.json());
 
 // get all cars
 app.get("/cars", (req, res) => {
-  getAllCars(req, res);
+  const result = getAllCars();
+  res.send(result);
 });
 
 // get single car
 app.get("/cars/:id", (req, res) => {
-  getSingleCar(req, res);
+  const id = parseInt(req.params.id);
+  let singleCar = getSingleCar(id);
+  res.status(200).send(singleCar);
 });
 
 // create new car
 app.post("/cars", (req, res) => {
-  createNewCar(req, res);
+  let newCar = {
+    id: db.length + 1,
+    make: req.body.make,
+    model: req.body.make,
+    colour: req.body.colour,
+    year: req.body.year
+  };
+  var madeNewCar = createNewCar(newCar);
+  res.status(200).send(madeNewCar);
 });
 
 // delete request
 app.delete("/cars/:id", (req, res) => {
-  deleteCar(req, res);
+  const id = parseInt(req.params.id);
+  let deleteMessage = deleteCar(id);
+  res.status(200).send(deleteMessage);
 });
 
 app.listen(PORT, () => {
--- express-api/requestLogics.js/requestLogic.js
+++ express-api/requestLogics.js/requestLogic.js
@@ -1,42 +1,24 @@
 import db from "../db/db";
 
-export const getAllCars = (req, res) => {
-  res.status(200).send({
-    cars: db
-  });
+export const getAllCars = () => {
+  return { cars: db };
 };
 
-export const getSingleCar = (req, res) => {
-  const id = parseInt(req.params.id);
-
+export const getSingleCar = id => {
   let singleCar = db.filter(car => car.id === id);
-  return res.status(200).send({
-    car: singleCar
-  });
+  return { car: singleCar };
 };
 
-export const createNewCar = (req, res) => {
-  let newCar = {
-    id: db.length + 1,
-    make: req.body.make,
-    model: req.body.make,
-    colour: req.body.colour,
-    year: req.body.year
-  };
-
+export const createNewCar = newCar => {
   db.push(newCar);
-  return res.status(200).send(newCar);
+  return newCar;
 };
 
-export const deleteCar = (req, res) => {
-  const id = parseInt(req.params.id);
-
+export const deleteCar = id => {
   db.map((car, index) => {
     if (car.id === id) {
       db.splice(index, 1);
-      return res.status(200).send({
-        message: "Car deleted successfuly"
-      });
     }
+    return { message: "Car deleted successfuly" };
   });
 };
