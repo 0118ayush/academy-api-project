111f06833c472a359cae1d03d209cb0e057b9605
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-12-12 13:30:43 +0000

    delete request implemented

--- express-api/app.js
+++ express-api/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const db = require("./db/db");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 5000;
+
+//Parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // get all cars
 app.get("/cars", (req, res) => {
@@ -13,7 +18,16 @@ app.get("/cars", (req, res) => {
 // delete request
 app.delete("/cars/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  console.log(id);
+
+  db.map((car, index) => {
+    if (car.id === id) {
+      db.splice(index, 1);
+      return res.status(200).send({
+        success: "true",
+        message: "Car deleted successfuly"
+      });
+    }
+  });
 });
 
 app.listen(PORT, () => {
